298df4b436ccba9a5cb50241fb9324b7aa5ea1e2
Vanderson Tavares <vandersontavares32@gmail.com>
2021-11-21 09:44:07 -0300

    Beckend project created

---
+++ beckend/src/main/java/com/devsuperior/dsdesafio/DsdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdesafioApplication.class, args);
+	}
+
+}
---
+++ beckend/src/test/java/com/devsuperior/dsdesafio/DsdesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
